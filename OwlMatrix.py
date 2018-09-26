3f2d0dec830d62b69f8b4ec6212108681489fbd1
Anton Kovalev <acetonen@gmail.com>
2018-09-26 10:30:37 +0300

    add more smart input in OwlMatrix

--- OwlMatrix.py
+++ OwlMatrix.py
@@ -13,26 +13,26 @@ x x x x    a b b a
 from sys import exit
 
 # input matrix
-print("You must insert SYMMETRIC matrix with ODD numbers of rows/columns.")
-rows = int(input("Iput ODD number of rows: "))
-print("input row by row, split values by spases:")
+print("""You must insert SYMMETRIC matrix with ODD numbers of rows/columns.
+Input row by row, split values by spases:""")
 matrix = []
-for i in range(rows):
+matrix.append(list(input("").split(' ')))
+for i in range(1, len(matrix[0])):
     matrix.append(list(input("").split(' ')))
 
 # ful protection
-if (len(matrix) != len(matrix[0])) or (rows % 2 or len(matrix[0]) % 2 != 0):
+if (len(matrix) != len(matrix[0])) or (len(matrix) % 2 or len(matrix[0]) % 2 != 0):
     exit("You must enter SYMMETRIC matrix with ODD numbers of rows/columns, goodbye.")
 
 # search in rows
-for i in range(rows):
-    for j in range(int(rows / 2)):
+for i in range(len(matrix)):
+    for j in range(int(len(matrix) / 2)):
         if matrix[i][j] != matrix[i][-1-j]:
             exit("Youre matrix doesn't Owl Matrix")
-            
+
 # search in columns
-for j in range(rows):
-    for i in range(int(rows / 2)):
+for j in range(len(matrix)):
+    for i in range(int(len(matrix) / 2)):
         if matrix[i][j] != matrix[-1-i][j]:
             exit("Youre matrix doesn't Owl Matrix")
 
